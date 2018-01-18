4672836563f24d167f8fd648db05062f03bd51e3
MASTER <sivaemail@gmail.com>
2018-01-18 13:57:27 -0500

    CallByValue and CallByName  Commit

--- src/main/scala/com/mastercoder/examples/CallByValueAndNameExample.scala
+++ src/main/scala/com/mastercoder/examples/CallByValueAndNameExample.scala
@@ -1,8 +1,32 @@
 package com.mastercoder.examples
 
-/**
-  * Created by MASTER on 1/18/2018.
-  */
-object CallByValueAndNameExample  {
+//1.CallByValue
+//2.CallByName
+object CallByValueAndNameExample extends App  {
+ val doWork:(Int => Int) = (i:Int) => { println("Start doing Work")
+                                 println(s"Received ${i}")
+   i+100
+ }
+
+  doWork(2)
+
+  //CallBy
+
+  def callByName(a: => Int): Unit = {
+    println("Inside Call By Name")
+
+    println(s"A =   ${a}")
+    println(s"A =   ${a}")
+  }
+
+  def callByValue(a:Int): Unit = {
+    println(s"A =   ${a}")
+    println(s"A =   ${a}")
+  }
+
+  callByValue(doWork(3))
+  callByName(doWork(3))
 
 }
+
+
