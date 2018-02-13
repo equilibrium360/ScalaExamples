20f2115a5049f02b7756912739ee08622f9a8e36
MASTER <sivaemail@gmail.com>
2018-02-13 11:17:37 -0500

    updated v3

--- src/main/scala/com/mastercoder/akka/examples/pipeline/PipelineSupervisor.scala
+++ src/main/scala/com/mastercoder/akka/examples/pipeline/PipelineSupervisor.scala
@@ -3,7 +3,7 @@ package com.mastercoder.akka.examples.pipeline
 import akka.actor.{Props, Actor}
 import com.typesafe.config.ConfigFactory
 import collection.JavaConverters._
-import scala.collection.mutable.ArrayBuffer
+import scala.collection.immutable.List
 
 /**
   * Created by UNIVERSE on 2/9/18.
@@ -13,14 +13,14 @@ class PipelineSupervisor extends Actor{
   val pipelineFlow = ConfigFactory.load().getConfig("pipelineFlow")
   val jobConfigList = pipelineFlow.getConfigList("jobSeq").asScala
 
-  val dependencyJobs = currentJobsLeft.filter(_.dependancyJobs.size !=0)
-  val doneJobs = ArrayBuffer[JobObject]()
-  val zeroDependencyJobsToStart = currentJobsLeft.filter(_.dependancyJobs.size == 0 )
+  var dependencyJobs = currentJobsLeft.filter(_.dependancyJobs.size !=0)
+  var doneJobs = List.empty[JobObject]
+  var zeroDependencyJobsToStart = currentJobsLeft.filter(_.dependancyJobs.size == 0 )
   val allJobsBuffer = jobConfigList.map(jc => new JobObject(jc))
 
-  val allJobs:ArrayBuffer[JobObject] = ArrayBuffer.empty[JobObject] ++= allJobsBuffer
+  val allJobs:List[JobObject] = List.empty[JobObject] ++ allJobsBuffer
 
-  val currentJobsLeft:ArrayBuffer[JobObject] = ArrayBuffer.empty[JobObject] ++= allJobsBuffer
+  var currentJobsLeft:List[JobObject] = List.empty[JobObject] ++ allJobsBuffer
 
   var allJobsCount = allJobs.size
   var completedJobsCount = 0
@@ -30,22 +30,42 @@ class PipelineSupervisor extends Actor{
 
   println(doneJobs.size)
 
-  //update currentJobsLeft
-  //get new list of  dependency jobs
-  //get new list of  zero Dependency Jobs
 
-  // Start Executing zero Dependency Jobs
-  //reset Zero Dependency jobs
+  def updateCurrentJobsLeft(compJob: JobObject):Unit = {
 
-/*  def updateDependencyJobs(compJob: JobObject):Unit = {
-
-    dependencyJobs.map(j => {
-        j.dependancyJobs-=compJob.uniqueId
+    currentJobsLeft = dependencyJobs.map(j => {
+      j.dependancyJobs-=compJob.uniqueId
+      j
     })
 
-  }*/
+  }
+
+  def areAllJobsDone():Boolean = {
+
+    completedJobsCount == allJobsCount
+
+  }
+  def updateDoneJobs(cj:JobObject):Unit = {
+    doneJobs =   doneJobs  :+ cj
+    completedJobsCount = completedJobsCount +1
+  }
+
+  def updateDependencyJobsList() = {
+
+    dependencyJobs = currentJobsLeft.filter(_.dependancyJobs.size !=0)
+
+  }
+
+  def updateZeroDependencyJobsList() = {
+
+    zeroDependencyJobsToStart = currentJobsLeft.filter(_.dependancyJobs.size == 0 )
+
+  }
+
+
+
  def resetZeroDependencyJobs():Unit = {
-   zeroDependencyJobsToStart.remove(0,zeroDependencyJobsToStart.size)
+   zeroDependencyJobsToStart = List.empty[JobObject]
 }
 
   def startZeroDependencyJobs():Unit = {
@@ -67,7 +87,31 @@ class PipelineSupervisor extends Actor{
     }
     case JobCompleted(comJb: JobObject) => {
 
-      doneJobs+=comJb
+      //update currentJobsLeft
+      //get new list of  dependency jobs
+      //get new list of  zero Dependency Jobs
+
+      // Start Executing zero Dependency Jobs
+      //reset Zero Dependency jobs
+
+      updateDoneJobs(comJb)
+      updateCurrentJobsLeft(comJb)
+      updateDependencyJobsList
+      updateZeroDependencyJobsList
+
+
+      //if allJobsDone
+
+      if(areAllJobsDone()) {
+        //shutdown System
+      }
+      startZeroDependencyJobs()
+      resetZeroDependencyJobs()
+
+
+
+
+
 
     }
     case _ => println("aaaaaaaa")
