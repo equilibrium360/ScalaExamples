eaa176cfec3349c081708e9a865a41154e9a4979
equation <sivasaiemail@gmail.com>
2018-02-19 21:50:55 -0500

    Collection Scenarios

--- src/main/scala/com/mastercoder/examples/CollectionsExamples.scala
+++ src/main/scala/com/mastercoder/examples/CollectionsExamples.scala
@@ -20,10 +20,44 @@ object CollectionsExamples extends App {
 
   case class Person(name:String, address:String, location:String)
 
+  //Adding elements to List
   val pList = List.empty[Person]
   val ptmp = Person("name1", "ad1", "c1") :: pList
 
-  ptmp.foreach(println)
+  val ptmp2 = Person("name2", "ad1", "c2") :: ptmp
+
+  val ptmp3 = ptmp2 :+ Person("fname3", "ad3", "c3")
+
+  ptmp3.foreach(println)
+
+  //Sorting elements in List
+  val sortedPersons = ptmp3.sortBy(_.name)
+
+  println("Sorted Persons")
+  sortedPersons.foreach(println)
+
+  //transform collection
+   val pset:Set[String] = sortedPersons.map(p => p.address).toSet
+
+  println("Transformed Persons")
+  pset.foreach(println)
+
+  //GroupBy Example
+
+  val groupedPersons = sortedPersons.groupBy(_.name.head)
+  println("Grouped Persons")
+  groupedPersons.foreach(println)
+
+ // Using Forloop on Collection
+
+  val pairs = for {
+    sperson <- sortedPersons
+    sloc <- sperson.location
+  } yield (sperson, sloc)
+
+  println("pairs"+ pairs)
+
+
 
 
 
